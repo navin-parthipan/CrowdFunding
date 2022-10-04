094f99c3d5335d1fe1c891a7cbae5c4e330d7c67
navin <navin.parthipan@gmail.com>
2022-10-04 19:15:20 +0530

    CrowdFunding backend deployed

---
+++ migrations/3_deploy_contract.js
@@ -0,0 +1,11 @@
+var CrowdFundingWithDeadline = artifacts.require("./CrowdFundingWithDeadline.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(
+    CrowdFundingWithDeadline, 
+    "Fund Raiser",
+    40,
+    10,
+    "0x77E7B172f1Cd81508bd7C252bCBf60028dD024A1"
+  );
+};
